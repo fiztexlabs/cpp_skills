51169c2dd8ade7ad1ec740ee7c73f790ac35a32f
fiztexlabs <fiztexlabs.science@yandex.ru>
2024-06-02 18:12:37 +0300

    Add m10

---
+++ module10/video1/src/cpp2py.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <pybind11/pybind11.h>
+
+
+int add(int i, int j) {
+    return i + j;
+}
+namespace py = pybind11;
+
+PYBIND11_MODULE(cpp2py, m) {
+    m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+
+        .. currentmodule:: cmake_example
+
+        .. autosummary::
+           :toctree: _generate
+
+           add
+           subtract
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+
+        Some other explanation about the subtract function.
+    )pbdoc");
+}
